b72b0d938ddba2ccef09d7640418eb77a8cb0959
faycel <debian2007.faical@gmail.com>
2019-12-07 01:53:25 +0100

    Using asyncTaskLoader unstead of AsyncTask

--- app/src/main/java/f/nouar/newsapp/MainActivity.java
+++ app/src/main/java/f/nouar/newsapp/MainActivity.java
@@ -1,34 +1,24 @@
 package f.nouar.newsapp;
 
 import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.AsyncTaskLoader;
 
-import java.text.DecimalFormat;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String GUARDIAN_API_URL = "https://content.guardianapis.com/search?api-key=6140f015-b070-4b9c-b1bf-160b8694dd7f";
+    public static final String GUARDIAN_API_URL = "https://content.guardianapis.com/search?q=debate&tag=politics/politics&from-date=2014-01-01&api-key=6140f015-b070-4b9c-b1bf-160b8694dd7f";
     private ListView listView;
     private AdapterNews adapter;
+    private LoaderManager.LoaderCallbacks<List<News>> mCallbacks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,10 +26,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         listView = findViewById(R.id.list);
-
-        adapter = new AdapterNews(this,0, new ArrayList<News>());
-        // Set the adapter on the {@link ListView}
-        // so the list can be populated in the user interface
+        adapter = new AdapterNews(this, 0, new ArrayList<News>());
         listView.setAdapter(adapter);
         /*listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -50,14 +37,12 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(websiteItent);
             }
         });*/
-
-        new asyncRequest().execute(GUARDIAN_API_URL);
-
+        new asyncLoader(this).forceLoad();
 
     }
 
 
-    class asyncRequest extends AsyncTask<String, Void, List<News>> {
+    /*class asyncRequest extends AsyncTask<String, Void, List<News>> {
         @Override
         protected List<News> doInBackground(String... strings) {
             if(strings.length<1 || strings[0] == null){
@@ -72,6 +57,29 @@ public class MainActivity extends AppCompatActivity {
             super.onPostExecute(newsList);
             adapter.clear();
             if(newsList != null && !newsList.isEmpty()){
+                adapter.addAll(newsList);
+            }
+
+        }
+    }*/
+
+    class asyncLoader extends AsyncTaskLoader<List<News>> {
+        public asyncLoader(@NonNull Context context) {
+            super(context);
+        }
+
+        @Nullable
+        @Override
+        public List<News> loadInBackground() {
+            List newsList = Utils.fetchNewsData(GUARDIAN_API_URL);
+            return newsList;
+        }
+
+        @Override
+        public void deliverResult(@Nullable List<News> newsList) {
+            super.deliverResult(newsList);
+            adapter.clear();
+            if (newsList != null && !newsList.isEmpty()) {
                 adapter.addAll(newsList);
             }
 
